4d6175f83a0db8ca82ab09f98927b9cb8c542e58
Solon-su <norton@emissaryholdings.co.uk>
2021-07-31 12:02:35 +0100

    Added play again function that runs for one loop Added function for main game logic Added goodbye banners

--- main.py
+++ main.py
@@ -34,56 +34,63 @@ def check_guess(usr_guess, cpu_guess):
     elif usr_guess == "scissors" and cpu_guess == "paper":
         return False
 
+def the_game():
+    guesses = ["rock", "paper", "scissors"]
+    game_count = 0
+    cpu_wins = 0
+    usr_wins = 0
+    cpu_guess = ""
+    usr_guess = ""
+    while cpu_wins < 3 and usr_wins < 3:
+        print("Game:", game_count + 1, "\n~~~~~~~")
+        usr_guess = input("Please enter your guess (rock, paper, scissors): ")
+        usr_guess.lower()
+        while usr_guess.lower() not in guesses:
+            usr_guess = input("Sorry, I didn't catch that. Enter again: ")
+        cpu_guess = random.choice(guesses)
+        print(". \n. \n. \n")
+        print("CPU guesses: ", cpu_guess.title(), "\n")
+        if check_guess(usr_guess, cpu_guess) == True:
+            print("{} beats {}. CPU wins.\n".format(cpu_guess.title(), usr_guess.title()))
+            cpu_wins += 1
+        elif check_guess(usr_guess, cpu_guess) == False:
+            print("{} beats {}. You win.\n".format(usr_guess.title(), cpu_guess.title()))
+            usr_wins += 1
+        else:
+            print("It's a tie:/\n")
+        print("SCORE: CPU {} | USER {}\n".format(cpu_wins, usr_wins))
+        game_count += 1
 
+        if cpu_wins >= 3:
+            go = pf.figlet_format(("Game Over! GG"))
+            print(go)
+            print("CPU Wins :(")
+            print("Better luck next time")
+
+        elif usr_wins >= 3:
+            winner = pf.figlet_format("WINNER!")
+            print(winner)
+            print("Congratulations!")
+            print("You win! :)")
+
+def play_again():
+    choices = ['y', 'n']
+    again = input("Play again? (y/n): ")
+    while again.lower() not in choices:
+        again = input("Please enter y or n: ")
+    if again.lower() == "y":
+        the_game()
+    else:
+        goodbye = pf.figlet_format("Goodbye, thanks for playing!")
+        print(goodbye)
 # game script
-guesses = ["rock", "paper", "scissors"]
-game_count = 0
-cpu_wins = 0
-usr_wins = 0
-cpu_guess = ""
-usr_guess = ""
+
 # play_again = True
 
 print_banner()
 print_welcome_message()
-# while play_again == True:
+the_game()
+play_again()
 
-while cpu_wins < 3 and usr_wins < 3:
-    print("Game:", game_count + 1, "\n~~~~~~~")
-    usr_guess = input("Please enter your guess (rock, paper, scissors): ")
-    usr_guess.lower()
-    while usr_guess.lower() not in guesses:
-        usr_guess = input("Sorry, I didn't catch that. Enter again: ")
-    cpu_guess = random.choice(guesses)
-    print(". \n. \n. \n")
-    print("CPU guesses: ", cpu_guess.title(), "\n")
-    if check_guess(usr_guess, cpu_guess) == True:
-        print("{} beats {}. CPU wins.\n".format(cpu_guess.title(), usr_guess.title()))
-        cpu_wins += 1
-    elif check_guess(usr_guess, cpu_guess) == False:
-        print("{} beats {}. You win.\n".format(usr_guess.title(), cpu_guess.title()))
-        usr_wins += 1
-    else:
-        print("It's a tie:/\n")
-    print("SCORE: CPU {} | USER {}\n".format(cpu_wins, usr_wins))
-    game_count += 1
 
-    if cpu_wins >= 3:
-        go = pf.figlet_format(("Game Over! GG"))
-        print(go)
-        print("CPU Wins :(")
-        print("Better luck next time")
-
-    elif usr_wins >= 3:
-        winner = pf.figlet_format("WINNER!")
-        print(winner)
-        print("Congratulations!")
-        print("You win! :)")
-# else:
-#     again = input("Play again? (y/n): ")
-#     if again.lower() == "y":
-#         cpu_wins = 0
-#         usr_wins = 0
-#         game_count = 0
-#         play_again = True
 
